3a7a5fbb6fcd891e1a8d96594d66bd07bf501c97
bhanumurthy1 <bhanupravallika123@gmail.com>
2022-08-28 23:10:52 -0400

    Caluculator.java

---
+++ Caluculator.java
@@ -0,0 +1,29 @@
+Class Demo
+{
+	int add(int a, int b)
+	{
+			return a+b;
+		}
+		int subtract(int a, int b)
+	{
+			return a-b;
+		}
+		int multiply(int a, int b)
+	{
+			return a*b;
+		}
+		int divide(int a, int b)
+	{
+			return a/b;
+		}
+
+	public static void main(String args[])
+	{
+		int a,b,c,d,e,f;
+		c=add(a,b);
+		d=subtract(a,b);
+		e=multiply(a,b);
+		f=divide(a,b);
+		System.out.println(“Addition:” + c + “Subtraction:” + d + “Multiplication:” + e + “Division:” + f)
+	}
+}
