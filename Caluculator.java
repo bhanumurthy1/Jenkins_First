d7072cddc5e0304b1e4203a59b88b95e899970ac
bhanumurthy1 <bhanupravallika123@gmail.com>
2022-09-03 01:45:54 -0400

    Added in the new Branch

---
+++ Caluculator.java
@@ -0,0 +1,36 @@
+
+Class Demo
+{
+	int add(int a, int b)
+	{
+			return a+b;
+		}
+		int subtract(int a, int b)
+	{
+			return a-b;
+			System.out.println("subtraction");
+                        System.out.println("jai balayya");
+		}
+		int multiply(int a, int b)
+	{
+			Systen.out.println(Multiplication);
+			return a*b;
+                        System.out.println("Added the Multiplication added by Bhanu");
+                        System.out.println("Added in New Branch");
+		}
+		int divide(int a, int b)
+	{
+			return a/b;
+		}
+
+	public static void main(String args[])
+	{
+		int a,b,c,d,e,f;
+		c=add(a,b);
+		d=subtract(a,b);
+		e=multiply(a,b);
+		f=divide(a,b);
+		System.out.println("Final Try is almost Done");
+		System.out.println(“Addition:” + c + “Subtraction:” + d + “Multiplication:” + e + “Division:” + f)
+	}
+}
