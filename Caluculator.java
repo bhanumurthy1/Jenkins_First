4af25dcfde0f4918f8ab256e2998e674c7ff1846
Yasaswini Chandolu <ychandolu@albany.edu>
2022-09-03 01:29:29 -0400

    Calculator in YELLOW

---
+++ Caluculator.java
@@ -0,0 +1,39 @@
+
+Class Demo
+{
+	int add(int a, int b)
+	{
+			return a+b;
+		}
+		int subtract(int a, int b)
+	{
+			return a-b;
+			System.out.println("subtraction");
+                        System.out.println("jai balayya");
+                       Sys tem.out println("jai ntr");
+		}
+		int multiply(int a, int b)
+	{
+			Systen.out.println(Multiplication);
+			return a*b;
+                        System.out.println("Added the Multiplication added by Bhanu");
+		}
+		int divide(int a, int b)
+	{
+			return a/b;
+			System.out.println(Division);
+		}
+
+	yellow for yashu
+
+	public static void main(String args[])
+	{
+		int a,b,c,d,e,f;
+		c=add(a,b);
+		d=subtract(a,b);
+		e=multiply(a,b);
+		f=divide(a,b);
+		System.out.println("Final Try is almost Done");
+		System.out.println(“Addition:” + c + “Subtraction:” + d + “Multiplication:” + e + “Division:” + f)
+	}
+}
