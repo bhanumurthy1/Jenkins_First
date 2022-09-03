2b4eafb5317eb530099169d59fb7f341fe021db4
vijayasaradhi99 <vponnaluri@albany.edu>
2022-09-03 01:09:44 -0400

    Added file to saradhi2

---
+++ Caluculator.java
@@ -0,0 +1,46 @@
+
+Class Demo
+{
+	int add(int a, int b)
+	{
+			return a+b;
+		}
+		int subtract(int a, int b)
+	{
+			return a-b;
+			System.out.println("subtraction");
+                        System.out.println("jai balayya");
+                       Sys tem.out println("jai ntr");
+		}
+		int multiply(int a, int b)
+	{
+			Systen.out.println(Multiplication);
+<<<<<<< HEAD
+			return a/b;
+		}
+		int divide(int a, int b)
+	{
+			return a*b;
+=======
+			return a*b;
+                        System.out.println("Added the Multiplication added by Bhanu");
+		}
+		int divide(int a, int b)
+	{
+			return a/b;
+			System.out.println(Division);
+>>>>>>> 6628feb2042072b32822e5c0c70dfbc3e5af0c0b
+		}
+	This is my branch
+
+	public static void main(String args[])
+	{
+		int a,b,c,d,e,f;
+		c=add(a,b);
+		d=subtract(a,b);
+		e=multiply(a,b);
+		f=divide(a,b);
+		System.out.println("Final Try is almost Done");
+		System.out.println(“Addition:” + c + “Subtraction:” + d + “Multiplication:” + e + “Division:” + f)
+	}
+}
